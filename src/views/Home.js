145164e465c8b5b14a4a43866f11edc537e83da1
Chie Stroud <urirunrunkoko@gmail.com>
2021-05-25 22:43:12 -0500

    adding border below navbar

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Typist from 'react-typist';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
 export default function Home() {
   return (
     <section className="aboutMe home">
-      <header className="title">Chie Stroud</header>
+      <header className="title"><Typist>Chie Stroud</Typist><Typist.Delay ms={500} /></header>
       <h1 className="jobTitle">Full Stack Web Developer</h1>
       <div className="btnContainer">
         <Button tag={Link} to='./about' id='btn'>About Me</Button>
